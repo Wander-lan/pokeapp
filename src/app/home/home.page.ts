75828b90a6161b3a7e65e29dec9cb7522c022661
wanderlan.alves <wanderlan.alves@mpdft.mp.br>
2025-06-19 14:37:38 -0300

    (feat): add basic home page with API services

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [CommonModule, IonicModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  pokemons: any[] = [];
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.pokemonService.getPokemons().subscribe((data) => {
+      this.pokemons = data;
+    });
+  }
 }
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  private baseUrl = 'https://pokeapi.co/api/v2';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemons(limit: number = 25, offset: number = 0): Observable<any[]> {
+    return this.http.get<any>(`${this.baseUrl}/pokemon?limit=${limit}&offset=${offset}`).pipe(
+      map((response) => {
+        return response.results.map((pokemon: any, index: number) => {
+          const id = offset + index + 1;
+          return {
+            name: pokemon.name,
+            id,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+          };
+        });
+      })
+    );
+  }
+}
